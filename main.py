155a35d0551a509e42365cb5206efe2d448aba04
Maks <macks.chygryn@icloud.com>
2024-09-24 12:41:17 +0300

    Оновлення main.py

--- main.py
+++ main.py
@@ -1,3 +1,38 @@
 import pygame
 
+# Initialize the game
+pygame.init()
 
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+
+display_width = 800
+display_height = 600
+
+gameDisplay = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption('Slither')
+
+def gameLoop():
+    gameExit = False
+
+    x1 = display_width / 2
+    y1 = display_height / 2
+
+    x1_change = 0
+    y1_change = 0
+
+    while not gameExit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameExit = True
+
+        gameDisplay.fill(white)
+        pygame.display.update()
+
+    pygame.quit()
+    quit()
+
+gameLoop()
